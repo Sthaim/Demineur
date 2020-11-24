a0964fbe59fa93e33a1ea88e09c12b6f6428d51a
DingerM GD1-2 <32112541+Sthaim@users.noreply.github.com>
2020-11-24 10:19:51 +0100

    Début de tableau

---
+++ main.py
@@ -0,0 +1,15 @@
+tabDem=[]
+tabSol=[]
+maxBombL=2
+maxBomb=10
+for i in range(5):
+    for j in range(5):
+        tabDem.append([i])
+        tabDem[i].append([j])
+        tabSol.append([i])
+        tabSol[i].append([j])
+        tabSol[i][j]=
+        tabDem[i][j]="X"
+print(TabDem)
+
+def affichage():
